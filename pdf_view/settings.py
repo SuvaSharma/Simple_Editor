e2c2b353efead8d528c8173eb28806f50f49bfda
SuvaSharma <sharmasuva1@gmail.com>
2023-10-11 12:55:30 +0545

    deployement commit

--- pdf_view/settings.py
+++ pdf_view/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kbv^3f&bs35l6yw5=%n%p!rn@w9v6sk+&&o67%g6s&-z2h8v+n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1' , 'localhost']
 
 
 # Application definition
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
